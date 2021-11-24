0c01aaf19c579e7dbec083875fca4bce622132cd
John <johnrubio93@gmail.com>
2021-11-24 21:33:16 +1100

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
 import Listings from './views/Listings';
+import Checkout from './views/Checkout';
 
 const App = () => {
   return (
     <div className="app">
-      <Listings />
+      <Nav />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Listings />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,5 @@
 const Nav = () => {
-  return (
-    <div className="nav">
-      
-    </div>
-  );
-}
+  return <div className="nav">Nav</div>;
+};
 
 export default Nav;
