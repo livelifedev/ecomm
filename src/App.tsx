89eb36edd43b7dc2168691c4d763f30ccc4db894
John <johnrubio93@gmail.com>
2021-11-24 23:01:09 +1100

    Create shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Checkout from './views/Checkout';
 
 const App = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto relative">
       <Nav />
       <Router>
         <Routes>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,21 @@
 const Nav = () => {
-  return <div className="nav">Nav</div>;
+  return (
+    <>
+      <div className="flex justify-between">
+        <a className="text-lg font-bold py-4">Home</a>
+        <button className="text-lg font-bold underline py-4">View Cart</button>
+      </div>
+
+      <div className="z-10 absolute right-0 border-solid border-2 border-black w-96 h-96 bg-white p-4">
+        <h3 className="font-bold text-center uppercase">Shopping Cart</h3>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Nav;
