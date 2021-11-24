2937a3301cf1c8da2cb9990f64fc59d759043258
John <johnrubio93@gmail.com>
2021-11-24 23:46:57 +1100

    Added cart logic

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,10 +1,23 @@
 import { ItemType } from '../types';
 
 const Item = (props: ItemType) => {
+  function addCartItem() {
+    const cart = localStorage.getItem('cart');
+    let newCart = [];
+
+    if (cart) {
+      newCart = JSON.parse(cart);
+    }
+
+    newCart.push(props);
+
+    localStorage.setItem('cart', JSON.stringify(newCart));
+  }
+
   return (
     <div className="flex my-16">
       <div className="w-60 relative">
-        <img src={props.image} />
+        <img src={props.image} alt={props.title} />
         <div className="absolute bottom-0 bg-red-600 px-4 py-2">
           <h4 className="text-white font-bold">${props.price.toFixed(2)}</h4>
         </div>
@@ -13,7 +26,10 @@ const Item = (props: ItemType) => {
         <h3 className="font-bold text-lg mb-2">{props.title}</h3>
         <p>{props.description}</p>
         <div className="pt-6">
-          <button className="bg-blue-500 text-white font-bold px-4 py-2 rounded">
+          <button
+            className="bg-blue-500 text-white font-bold px-4 py-2 rounded"
+            onClick={addCartItem}
+          >
             Add to Cart
           </button>
         </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,49 @@
+import { useState } from 'react';
+import { ItemType } from '../types';
+
 const Nav = () => {
+  const [showCart, setShowCart] = useState(false);
+  const [items, setItems] = useState([] as ItemType[]);
+
+  function getCartItems() {
+    const cart = localStorage.getItem('cart');
+
+    if (cart) {
+      setItems(JSON.parse(cart));
+    }
+  }
+
   return (
     <>
       <div className="flex justify-between">
         <a className="text-lg font-bold py-4">Home</a>
-        <button className="text-lg font-bold underline py-4">View Cart</button>
+        <button
+          className="text-lg font-bold underline py-4"
+          onClick={() => {
+            getCartItems();
+            setShowCart(!showCart);
+          }}
+        >
+          View Cart
+        </button>
       </div>
 
-      <div className="z-10 absolute right-0 border-solid border-2 border-black w-96 h-96 bg-white p-4">
-        <h3 className="font-bold text-center uppercase">Shopping Cart</h3>
-        <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-        </ul>
-      </div>
+      {showCart && (
+        <div className="z-10 absolute right-0 border-solid border-2 border-black w-96 h-96 bg-white p-4">
+          <h3 className="font-bold text-center uppercase">Shopping Cart</h3>
+          <ul>
+            {items.map((item, i) => (
+              <li key={`${item.id}${i}`} className="my-2">
+                {item.title}
+              </li>
+            ))}
+          </ul>
+
+          <a className="text-lg font-bold py-4 absolute bottom-0 right-6">
+            Checkout
+          </a>
+        </div>
+      )}
     </>
   );
 };
